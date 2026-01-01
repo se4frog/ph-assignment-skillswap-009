ce538eb5029b59b2bf22952612fe75649582279b
se4frog <siddiqurrahman3013@email.com>
2026-01-01 23:11:47 +0600

    first commit

---
+++ src/routes/Router.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import HomeLayout from '../layouts/HomeLayout';
+
+const Router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomeLayout></HomeLayout>
+    }
+])
+
+export default Router;
